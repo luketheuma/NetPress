109f3396db70a28d05abeb0c953cfb83c07664f8
Etienne Galea <egalea.11@gmail.com>
2016-06-03 12:16:07 +0200

    added homepage theme - blog list

--- NetPress/NetPress/Controllers/BlogsController.cs
+++ NetPress/NetPress/Controllers/BlogsController.cs
@@ -50,6 +50,7 @@ namespace NetPress.Controllers
         {
             if (ModelState.IsValid)
             {
+                var userid = db.AspNetUsers.Where(u => u.UserName == User.Identity.Name).FirstOrDefault().Id;
                 db.Blogs.Add(blog);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -123,5 +124,14 @@ namespace NetPress.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult nextItem()
+        {
+            List<Blog> b = db.Blogs.ToList();
+
+            return PartialView("_item", b );
+        }
+        
+
     }
 }
