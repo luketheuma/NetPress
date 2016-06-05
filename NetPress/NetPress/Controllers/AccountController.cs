be5d0bb03d673bf58bc9a745e56dea7c29b959e0
Luke Theuma <luketheuma9@gmail.com>
2016-06-05 19:47:34 +0200

    login redirection fixed swagggg

--- NetPress/NetPress/Controllers/AccountController.cs
+++ NetPress/NetPress/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace NetPress.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Blogs");
+                    return RedirectToAction("UserBlogs", "Blogs");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace NetPress.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Blogs");
         }
 
         //
